7240c5f39fa0266a1c8c3e03ac3201b701d4fd9f
Kenny Taylor <kennytaylor@runbox.com>
2019-10-18 04:53:06 +0000

    Fixed API code to return -1 if URL is in database but is unscored

--- web/api/plugin-submit-url.php
+++ web/api/plugin-submit-url.php
@@ -14,13 +14,17 @@
         mysqli_select_db($link, "analytics") or die("Could not select database\n");
 
         // Fetch the page score
-        $query = "SELECT Page_Score FROM pages WHERE URL = '" . $_REQUEST["url"] . "'";
+        $query = "SELECT Page_Score FROM pages WHERE URL = '" . $_REQUEST["url"] . "' AND NOT Page_Score IS NULL";
         $result = mysqli_query($link, $query);
         $numrows = mysqli_num_rows($result);
         if ($numrows) {
+            // Page score exists in database
             $row = mysqli_fetch_row($result);
             $pagescore = $row[0];
         } else {
+            // Page score does not exist in database--call analyzer
+            $output = exec("python analyzer.py " . $_REQUEST["url"]);
+            print $output;
             $pagescore = -1;
         }
 
