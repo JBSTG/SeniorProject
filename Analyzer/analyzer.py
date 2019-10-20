e98c9f8e84d383c98a3c5803d67dd8f66ad2f19f
Adrian Gutierrez <46102354+agutierrez63@users.noreply.github.com>
2019-10-20 16:08:55 -0700

    Update analyzer.py

--- Analyzer/analyzer.py
+++ Analyzer/analyzer.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
@@ -49,51 +48,3 @@ for learning_rate in lr_list:
     gb_clf.fit(X_train, y_train)
     print("Learning Rate: ", learning_rate)
     print("Accuracy score (training): {0:.3f}".format(gb_clf.score(X_train, y_train)))
-=======
-#!/usr/bin/python3
-from lxml.html import fromstring
-import urllib
-import requests
-import sys
-
-'''
-# Open the file to read
-dataFile = open('clickbait_data.csv', encoding='utf-8')
-
-# read the data from file into a list
-listOfLines = dataFile.read().splitlines()
-aLine = listOfLines[1]
-lineItems = aLine.split(',')
-print(lineItems)
-value = lineItems[1]
-
-sum = 0
-count = 0
-listOfAverages = []
-output = int(float(value))
-
-for i in range(1, len(listOfLines), 1):
-    aLine = listOfLines[i]
-    lineItems = aLine.split(',')
-    value = lineItems[1]
-    count = count + 1
-    sum = sum + output
-    avg = sum / count
-    if i == 82:
-        subList = [avg]
-        listOfAverages.append(subList)
-        sum = int(float(value))
-        count = 1
-for i in range(0, len(listOfAverages), 1):
-    print(listOfAverages[i])
-'''
-
-# function will grab title and all links in url
-def grabURL(url):
-    r = requests.get(url)
-    tree = fromstring(r.content)
-    print (tree.findtext(".//title"))
-    print (tree.xpath("//a/@href"))
-
-grabURL(sys.argv[1])
->>>>>>> 9ba7af88245e0df13350e765fcb9407bae220317
