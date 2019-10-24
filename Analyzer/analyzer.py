b92f3f8faddeb04de460c3d6f1ed99bab6b927df
Adrian Gutierrez <46102354+agutierrez63@users.noreply.github.com>
2019-10-24 15:58:51 -0700

    Updated analyzer
    returns score from database

--- Analyzer/analyzer.py
+++ Analyzer/analyzer.py
@@ -1,6 +1,7 @@
 from lxml.html import fromstring
 import urllib
 import random
+import re
 import requests
 import sys
 
@@ -20,7 +21,8 @@ lineItems = aLine.split(",")
 sum = 0
 count = 0
 listOfAverages = []
-for i in range(1, 10, 1):
+
+for i in range(1, len(listOfLines), 1):
     aLine = listOfLines[i]
     lineItems = aLine.split(",")
     # set id to Num column
@@ -35,25 +37,49 @@ for i in range(1, 10, 1):
     #subList = [count, avg]
     #listOfAverages.append(subList)
 
-#print("Score: {:0.2f}\n".format(avg))
+# print("Score: {:0.2f}\n".format(avg))
+
+# pattern:   number              noun                      verb             word char
+# regex = "(?:\s*\d+)*(?:\b[A-Z][A-Za-z0-9]+\b)(?:\s*\b[A-Z][A-Za-z0-9]+\b)(?:\s+\w++)?"
+# def return score(avg, current_score)
+#   function will calculate the score
+#   to ifentify whether the article
+#   is clickbait
+
+# function will give approximate match percentage
+# this is known as fuzzy string matching
+def percent_match(regex, target):
+    left = 0
+    right = len(target) - 1
+    current = right / 2
+    while left < right:
+        if regex.match(target[left:right]):
+            left = current
+        else:
+            right = current
+        current = (right - left) / 2
+    return m / len(target)
+    #return_score(avg, m)
 
 # look for title in csv file and return score if
 # available, else return -1
 def check_title(title):
     with open("clickbait_data.csv", "r") as f:
         s = f.read()
+        data = lineItems[len(lineItems)-2]
         if title not in s:
             # not in database
             print("Score:", -1, "\n")
+            # percent_match(regex, title)
         else:
             # will print clickbait score of article
-            print("Score:", 0, "\n")
+            print("Score:", data, "\n")
 
 # function will grab title and all links in url
 def grabURL(url):
     r = requests.get(url)
     tree = fromstring(r.content)
-    # articletitle
+    # article title
     title = tree.findtext(".//title")
     # list of links in url
     ahref = tree.xpath("//a/@href")
@@ -71,4 +97,4 @@ except:
     # inform you that no argument has been provided
     print("No URL in command line")
     # exit 
-    sys.exit(0)
+    sys.exit(1)
