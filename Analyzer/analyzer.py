27a7717b93e2718fdf33810939721f568f2d5b9d
Adrian Gutierrez <46102354+agutierrez63@users.noreply.github.com>
2019-10-23 23:08:30 -0700

    Various Fix
    removed an unecessary comma

--- Analyzer/analyzer.py
+++ Analyzer/analyzer.py
@@ -5,7 +5,7 @@ import requests
 import sys
 
 # Open the file to read
-dataFile = open('clickbait_data.csv', encoding='utf-8')
+dataFile = open("clickbait_data.csv", encoding="utf-8")
 
 # read the data from file into a list
 listOfLines = dataFile.read().splitlines()
@@ -15,13 +15,14 @@ aLine = listOfLines[1]
 
 # when printed list will look like following:
 # ['Num', 'Clickbait', 'Title']
-lineItems = aLine.split(',')
+lineItems = aLine.split(",")
+# initialize
 sum = 0
 count = 0
 listOfAverages = []
 for i in range(1, 10, 1):
     aLine = listOfLines[i]
-    lineItems = aLine.split(',')
+    lineItems = aLine.split(",")
     # set id to Num column
     id = lineItems[0]
     # set data to clickbait column
@@ -30,32 +31,36 @@ for i in range(1, 10, 1):
     # sum of data
     sum = sum + float(data)
     avg = sum / count
-    #print('id=', id, 'sum=', sum, 'avg=', avg)
+    #print("id=", id, "sum=", sum, "avg=", avg)
     #subList = [count, avg]
     #listOfAverages.append(subList)
 
 #print("Score: {:0.2f}\n".format(avg))
 
 # look for title in csv file and return score if
-# avaialable, else return -1
+# available, else return -1
 def check_title(title):
-    with open('clickbait_data.csv', 'r') as f:
+    with open("clickbait_data.csv", "r") as f:
         s = f.read()
         if title not in s:
-            print("Score:", -1)
+            # not in database
+            print("Score:", -1, "\n")
         else:
-            print("Title already in database")
+            # will print clickbait score of article
+            print("Score:", 0, "\n")
 
 # function will grab title and all links in url
 def grabURL(url):
     r = requests.get(url)
     tree = fromstring(r.content)
+    # articletitle
     title = tree.findtext(".//title")
+    # list of links in url
     ahref = tree.xpath("//a/@href")
     # call function to check for title
     check_title(title)
     # prints article title
-    print(title, "\n")
+    print("Title:", title, "\n")
     # prints list of links in url
     print(ahref)
 
