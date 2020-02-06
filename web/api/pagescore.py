55507f596054b3ab781268da53f0343b4dbd712f
Adrian Gutierrez <46102354+agutierrez63@users.noreply.github.com>
2020-02-05 21:05:47 -0800

    Updated pagescore.

--- web/api/pagescore.py
+++ web/api/pagescore.py
@@ -1,109 +1,141 @@
+from urllib.request import urlparse, urljoin
 from bs4 import BeautifulSoup
-from urllib.parse import urlsplit
-from urllib.parse import urlparse
 from collections import deque
 import requests
 import requests.exceptions
 import subprocess
 import argparse
+import signal
+import logging
 import re
 import sys
 import os
 
+class TimeoutException(Exception):
+    pass
+
+def timeout_handler(signum, frame):
+    raise TimeoutException
+
+signal.signal(signal.SIGALRM, timeout_handler)
+
+formatter = logging.Formatter("%(asctime)s-%(levelname)s-%(process)s-%(message)s")
 # url for testing: "https://scrapethissite.com/"
-# set of urls that have been processed
-processed_urls = set()
 
-# set of domains inside the target webiste
-local_urls = set()
+# set default score
+score = 0.0
 
-# set of domains outside of the target website
-foreign_urls = set()
+# initialize the set of links (unique links)
+internal_urls = set()
+external_urls = set()
 
-# set of broken urls
-broken_urls = set()
+total_urls_visited = 0
 
-# set the base case score default
-t = 1
+def is_valid(url):
+    """
+    Checks whether `url` is a valid URL.
+    """
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
 
-# set variable for number of urls
-nURLs = 0
 
-def grab_url(domain):
-    # queue of urls to be crawled next
-    new_urls = deque([domain])
+def get_all_website_links(url):
+    """
+    Returns all URLs that is found on `url` in which it belongs to the same website
+    """
+    # all URLs of `url`
+    urls = set()
+    # domain name of the URL without the protocol
+    domain_name = urlparse(url).netloc
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    for a_tag in soup.findAll("a"):
+        href = a_tag.attrs.get("href")
+        if href == "" or href is None:
+            # href empty tag
+            continue
+        # join the URL if it's relative (not absolute link)
+        href = urljoin(url, href)
+        parsed_href = urlparse(href)
+        # remove URL GET parameters, URL fragments, etc.
+        href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
+        if not is_valid(href):
+            # not a valid URL
+            continue
+        if href in internal_urls:
+            # already in the set
+            continue
+        if domain_name not in href:
+            # external link
+            if href not in external_urls:
+                # print(f"{GRAY}[!] External link: {href}{RESET}")
+                external_urls.add(href)
+            continue
+        # print(f"{GREEN}[*] Internal link: {href}{RESET}")
+        urls.add(href)
+        internal_urls.add(href)
+    return urls
 
-    # use global nURLs to midify global variable counter
-    global nURLs
 
-    try:
-        # process urls one by one until queue is exhausted
-        while len(new_urls):
-            # move url from queue to processed url set
-            url = new_urls.popleft()
-            processed_urls.add(url)
-
-            # print the current url
-            # print("Processing %s" % url)
-            try:
-                response = requests.get(url)
-            except(requests.exceptions.MissingSchema, requests.exceptions.ConnectionError,
-                    requests.exceptions.InvalidURL, requests.exceptions.InvalidSchema):
-                # add broken urls to its own set
-                broken_urls.add(url)
-                continue
-            # extract base url to resolve relative links
-            parts = urlsplit(url)
-            base = "{0.netloc}".format(parts)
-            strip_base = base.replace("www.", "")
-            base_url = "{0.scheme}://{0.netloc}".format(parts)
-            path = url[:url.rfind('/')+1] if "/" in parts.path else url
-
-            # using beautiful soup for html document
-            soup = BeautifulSoup(response.text, "lxml")
-
-            for link in soup.find_all("a"):
-                # extracting link url from tag
-                anchor = link.attrs["href"] if "href" in link.attrs else ""
-                if anchor.startswith("/"):
-                    local_link = base_url + anchor
-                    local_urls.add(local_link)
-                elif strip_base in anchor:
-                    local_urls.add(anchor)
-                elif not anchor.startswith("http"):
-                    local_link = path + anchor
-                    local_urls.add(local_link)
-                else:
-                    foreign_urls.add(anchor)
-                new_urls.append(link)
-                nURLs = len(new_urls)
-            set_rank(nURLs, t)
-    except KeyboardInterrupt:
-        print(" Keyboard Interrupt\n")
-        sys.exit()
-
-def set_rank(url, t):
-    if url is 0:
-        score = -1.0
+def crawl(url, max_urls):
+    """
+    Crawls a web page and extracts all links.
+    You'll find all links in `external_urls` and `internal_urls` global set variables.
+    params:
+        max_urls (int): number of max urls to crawl, default is 10.
+    """
+    global total_urls_visited
+    total_urls_visited += 1
+    links = get_all_website_links(url)
+    for link in links:
+        if total_urls_visited > max_urls:
+            break
+        crawl(link, max_urls=max_urls)
+    if len(external_urls) is 0:
+        score = 0.0
     else:
-        sum = (1 - (t - 1)) / url
-        score = (((1 - sum) * t) - sum) / 2
-    if score < 0:
-        score = score * -1
-    print("Score:", round(score, 2))
+        rank = len(internal_urls) / len(external_urls)
+        score = rank / max_urls
+        return score
 
 def main(argv):
-    title = "Data Dogs - Senior Project Page Score Ranker"
+    title = "Data Dogs Analytics - Senior Project Page Score Ranker"
     parser = argparse.ArgumentParser(description=title)
     # passing flags
-    parser.add_argument("--domain", "-d", required=True,
-            help="domain name of the website you want to give a page score.")
+    parser.add_argument("--domain", "-d", required=True, help="domain name of the website.")
     parser.parse_args()
 
     # read arguments from the command line
     args = parser.parse_args()
     domain = args.domain
-    grab_url(domain)
+    for i in range(1):
+        signal.alarm(10)
+        try:
+            # start page rank process
+            score = crawl(domain, max_urls=1)
+            print("Score:", round(score, 2))
+        except TimeoutException as e:
+            formatter = logging.Formatter("%(asctime)s-%(levelname)s-%(process)s-%(message)s")
+            # error logger
+            error_logger = setup_logger("error_logger", "./logs/datadogs_pagescore_error_logfile.log")
+            error_logger.info("Error occurred")
+
+            # exception error logger
+            exception_logger = setup_logger("exception_logger", "./logs/datadogs_pagescore_exception_logfile.log")
+            exception_logger.exception("Timeout exception occurred")
+
+            continue
+        else:
+            signal.alarm(0)
+
+def setup_logger(name, log_file, level=logging.INFO):
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
 
 if __name__ == "__main__":
     main(sys.argv[1:])
