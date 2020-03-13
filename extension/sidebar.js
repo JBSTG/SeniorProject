c5cbe14c59ba56781f7349380a118cddcfeac5da
jbstg <igearpro@yahoo.com>
2020-03-13 16:02:08 -0700

    Added live comments to extension

--- extension/sidebar.js
+++ extension/sidebar.js
@@ -1,7 +1,11 @@
+var currentArticleId = -1;
+var commentServerConnection2 = new WebSocket("wss://www.datadogsanalytics.com:9000");
+
 function handleMessage(request, sender, sendResponse) {
   if(request.context=="explore"){
     var url = request.URL;
-    var sReq = new XMLHttpRequest();
+	var sReq = new XMLHttpRequest();
+	
     sReq.open("POST","https://datadogsanalytics.com/getArticleInfoAndComments.php",true);
     sReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     sReq.send("url="+url);
@@ -9,7 +13,8 @@ function handleMessage(request, sender, sendResponse) {
     sReq.onload = function(){
       var newURL = "{\""+url+"\":"+this.responseText+"}";
       console.log(this.responseText);
-      newURL = JSON.parse(newURL);
+	  newURL = JSON.parse(newURL);
+	  currentArticleId = newURL[url].id;
       document.getElementById("pageTitle").innerHTML = newURL[url].title;
       document.getElementById("siteScore").innerHTML = "Site Score: " + newURL[url].site_score+"%";
       document.getElementById("commentsHeader").style.visibility="visible";
@@ -21,30 +26,46 @@ function handleMessage(request, sender, sendResponse) {
 		document.getElementById("pageTitle").parentElement.classList.add("w3-blue");
 	  }
 	  
-	  var commentsBox = document.getElementById("comments");
+	  var commentsBox = document.getElementById("infoBoxComments");
 	  commentsBox.innerHTML = "";
 	  if(newURL[url].comments.length==0){
 		commentsBox.innerHTML = "<div class=\"w3-container w3-text-blue-gray\"><p>No comments yet.</p></div>";
       }else{
 		for(var i = 0;i<newURL[url].comments.length;i++){
-			var h = document.createElement("div");
-			h.className="w3-container w3-light-blue w3-text-white";
-			var u = document.createElement("p");
-			u.innerHTML = newURL[url].comments[i].username;
-			h.appendChild(u);
-			commentsBox.appendChild(h);
-			
-			var c = document.createElement("div");
-			c.className="w3-container w3-text-pale-blue";
-			var p = document.createElement("p");
-			p.innerHTML = newURL[url].comments[i].body;
-			
-			
-			c.appendChild(p);
-			commentsBox.appendChild(c);
+			addComment(newURL[url].comments[i].username,newURL[url].comments[i].body,newURL[url].comments[i].date);
 		}
 	  }
     }
   }
 }
+
+commentServerConnection2.onopen = function(e) {
+	console.log("Connected to comment server.");
+	document.getElementById("submitCommentButton2").addEventListener("click",function(){
+	  var commentMessage = new Object();
+	  commentMessage.isCommentMessage = true;
+	  commentMessage.body = document.getElementById("commentEntry").value;
+	  document.getElementById("commentEntry").value = "";
+	  commentMessage.user = 9;
+	  commentMessage.username = "Anonymous";
+	  commentMessage.article = currentArticleId;
+	  console.log(commentMessage);
+	  commentServerConnection2.send(JSON.stringify(commentMessage));
+  });
+}
+function addComment(username,body,date){
+    //This is because I am too lazy to implement synchonicity in the server.
+    //Really no big deal, the right date shows up on page refresh.
+    if(date==undefined){
+      date="Just Now";
+    }
+    document.getElementById("infoBoxComments").innerHTML += username+": "+body+" "+date+"<br>";
+}
+
+commentServerConnection2.onmessage = function(message) {
+	var msg = JSON.parse(message.data);
+	if(msg.article==currentArticleId){
+	  addComment(msg.username,msg.body,msg.date);
+	}
+};
 browser.runtime.onMessage.addListener(handleMessage);
